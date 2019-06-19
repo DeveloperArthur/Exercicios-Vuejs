1254754a450d45ee83ad0fd0d5f6ebbf38f58a7e
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-19 09:55:13 -0300

    Delete resposta.js

--- resposta.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-  el: "#exercise",
-  data: {
-    name: 'Arthur',
-    age: 20,
-    link: 'https://cdn.pixabay.com/photo/2018/07/23/22/48/nature-3558033_960_720.jpg',
-    teste: 'Arthur'
-  }
-})
