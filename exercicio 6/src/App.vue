a043f0d0accc2c4f3073a0e3f02e0ca67db6fbdb
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-24 15:30:30 -0300

    Add files via upload

---
+++ exercicio 6/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <app-meio></app-meio>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import header from './components/header.vue';
+import meio from './components/meio.vue';
+import footer from './components/footer.vue';
+
+export default {
+    components: {
+        AppHeader: header,
+        AppMeio: meio,
+        AppFooter: footer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ exercicio 6/src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <header>
+                <h1>Server Status</h1>
+            </header>
+        </div>
+    </div>
+</template>
